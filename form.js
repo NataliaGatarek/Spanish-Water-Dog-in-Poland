16a6386cc483b4e87a6e211e4e733bb2c321403c
Nataliag <nova.nathalia@gmail.com>
2021-04-02 19:13:37 +0200

    js 2nd commit

--- form.js
+++ form.js
@@ -1,12 +1,15 @@
 function displayUserInfo() {
   let params = new URL(document.location).searchParams;
-  let firstname = params.get("firstname");
-  //let email = parms.get("email");
-  console.log("firstname", firstname);
- //console.log("email", email);
-  document.getElementById("user.name").innerHTML = firstname;
-  
+  let name = params.get("name");
+  console.log("name", name);
+    document.getElementById("user.name").innerHTML = name;
+      let lastname = params.get("lastname");
+  console.log("lastname", lastname);
+    document.getElementById("user.name").innerHTML = lastname;
 }
+
+
+
   let pageTitle = "THANKYOU";
 console.log(`document.title`, document.title);
 //  DECIDING WHEN TO RUN displayUserInfo() BASED ON THE TITLE OF THE PAGE
