30353d90ca4fa884287fba11aa3d72f70aac6596
Nataliag <nova.nathalia@gmail.com>
2021-04-04 18:36:40 +0200

    4th js commit

--- form.js
+++ form.js
@@ -4,7 +4,17 @@ function displayUserInfo() {
   console.log("name", name);
   let lastname = params.get("lastname");
   console.log("lastname", lastname);
-  document.getElementById("user.name").innerHTML = lastname; name;
+  document.getElementById("user.name").innerHTML = name;
+  document.getElementById("user.details").innerHTML = lastname; 
+  let age = parseInt(params.get('age'))
+  console.log("age", age);
+  document.getElementById("user.age").innerHTML = age;
+  let have = params.get('have');
+  console.log("have", have);
+  let many = parseInt(params.get('many'))
+  console.log("many", many)
+  document.getElementById("user.many").innerHTML = many;
+  document.getElementById("user.have").innerHTML = have;
 }
 
 
